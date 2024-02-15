769ba93184dd6150bfb8ef567e5d217127b964ea
GutuShiferaw <gutuwestbrook@gmail.com>
2024-02-14 21:50:27 -0500

    update

--- client/src/pages/Gift.jsx
+++ client/src/pages/Gift.jsx
@@ -1,20 +1,27 @@
 import React from "react";
-import Vid from "../assets/vid.mp4";
+
+// Replace this with your YouTube video ID
+const youtubeVideoId = "Gil5euOkl4M";
+
 export default function Gift() {
   return (
     <div className="flex gift-cont h-screen w-screen">
       <div className="flex items-center justify-center w-full">
         <div className="video-window text-center">
-          <video width="100%" height="100%" controls className=" mb-8">
-            <source src={Vid} />
-            Your browser does not support the video tag.
-          </video>
+          <iframe
+            width="100%"
+            height="100%"
+            src={`https://www.youtube.com/embed/${youtubeVideoId}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allowFullScreen
+            className=" mb-8"
+          ></iframe>
           <a
             href="/"
-            className=" text-2xl text-red-600 font-bold bg-white bg-opacity-60 p-2 rounded-xl hover:bg-opacity-90"
+            className="text-2xl text-red-600 font-bold bg-white bg-opacity-60 p-2 rounded-xl hover:bg-opacity-90"
           >
-            {" "}
-            Go back to Home{" "}
+            Go back to Home
           </a>
         </div>
       </div>
